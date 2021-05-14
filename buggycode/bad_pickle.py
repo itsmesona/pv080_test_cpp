c3bfa0c2ce3c82f87fe5c2ef6b5b3384b669d643
Soňa Nemečkayová <xnemeck@fi.muni.cz>
2021-05-14 09:18:47 +0200

    3

--- buggycode/bad_pickle.py
+++ buggycode/bad_pickle.py
@@ -13,24 +13,24 @@ import cPickle
 Input injection
 """
 def transcode_file(request, filename):
-	command = 'ffmpeg -i "{source}" output_file.mpg'\
-	.format(source=filename)
-	subprocess.call(command, shell=True)  # a bad idea!
+    command = 'ffmpeg -i "{source}" output_file.mpg'\
+    .format(source=filename)
+    subprocess.call(command, shell=True)  # a bad idea!
 
 """
 # Assert statements
 """
 def foo(request, user):
-	assert user.is_admin, 'user does not have access'
-	# secure code...
+    assert user.is_admin, 'user does not have access'
+    # secure code...
 
 
 """
 Pickles
 """
 class RunBinSh(object):
-	def __reduce__(self):
-		return (subprocess.Popen, (('/bin/sh',),))
+    def __reduce__(self):
+        return (subprocess.Popen, (('/bin/sh',),))
 
 
 print(base64.b64encode(cPickle.dumps(RunBinSh())))
