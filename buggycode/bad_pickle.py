2d16045fa9059347fd852d9ae20f521854b29fe0
Soňa Nemečkayová <xnemeck@fi.muni.cz>
2021-05-14 09:17:52 +0200

    2

--- buggycode/bad_pickle.py
+++ buggycode/bad_pickle.py
@@ -1,28 +1,33 @@
+"""
+idk 
+"""
 # contains bunch of buggy examples
-# taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+# taken from https://hackernoon.com/10-common-security-gotchas
+#-in-python-and-how-to-avoid-them-e19fbe265e03
 import base64
+import subprocess
 import cPickle
-import subprocess
-import subprocess
+
 
 """
-high level support for doing this and that.
+Input injection
 """
-
-# Input injection
 def transcode_file(request, filename):
 	command = 'ffmpeg -i "{source}" output_file.mpg'\
 	.format(source=filename)
 	subprocess.call(command, shell=True)  # a bad idea!
 
-
+"""
 # Assert statements
+"""
 def foo(request, user):
 	assert user.is_admin, 'user does not have access'
 	# secure code...
 
 
-# Pickles
+"""
+Pickles
+"""
 class RunBinSh(object):
 	def __reduce__(self):
 		return (subprocess.Popen, (('/bin/sh',),))
